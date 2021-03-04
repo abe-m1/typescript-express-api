ab5cff4876cccb60114a1de70cd817568b97b9ad
abe menashy <abestbet1@gmail.com>
2021-03-04 10:53:18 -0800

    current user route

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,9 +1,27 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('hi there')
+  //(!req.session?.jwt) is equivalent of 
+  //(!req.session || !req.session.jwt)
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  //decode jwt
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_SECRET!
+    );
+    // if payload, send back currentUser
+    res.send({ currentUser: payload });
+
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 // rename at same time as exporting
