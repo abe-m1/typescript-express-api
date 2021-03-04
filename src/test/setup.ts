7c31502fdcf68211912271ee79f32a21ff67b1f5
abe menashy <abestbet1@gmail.com>
2021-03-04 14:17:18 -0800

    test current user request handler

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,8 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
 
 let mongo: any;
 
@@ -28,3 +30,27 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 })
+
+
+//if decide to change from global , can just export function
+//and import it where needed
+
+export const signup = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email, password
+    })
+    .expect(201)
+
+  //cookie does not get included in subsequent responses, so we will save it
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie
+  // to use:
+  //const cookie = await signup()
+}
