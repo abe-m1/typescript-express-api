e1816009b08b6a6a3365ed0758b2753113bd1e0d
abe menashy <abestbet1@gmail.com>
2021-03-03 18:27:04 -0800

    common format for errors

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,12 +1,29 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
 
 export const errorHandler = (
   err: Error, 
   req: Request, 
   res: Response, 
   next: NextFunction) => {
-    console.log('something went wrong')
+    if (err instanceof RequestValidationError){
+      const formattedErrors = err.errors.map(error => {
+        //transforming error into the following format
+        return {
+          message: error.msg,
+          fields: error.param
+        }
+      })
+      return res.status(400).send({ errors: formattedErrors});
+    }
+    if (err instanceof DatabaseConnectionError){
+      return res.status(500).send({ errors: [
+        { message: err.reason}
+      ]})
+    }
     res.status(400).send({
-      message: err.message
-    })
+      errors: [{ message: 'Something went wrong'}]
+    });
 }
